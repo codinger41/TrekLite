b412f489daa387b507233f3dd5bf482f501854de
Olamilekan Ibrahim <leksyib14@gmail.com>
2019-10-02 13:15:35 +0100

    Update pubnub.ts

--- src/utils/pubnub.ts
+++ src/utils/pubnub.ts
@@ -1,8 +1,8 @@
 import PubNub from 'pubnub'
 
 const pubnub: any = new PubNub({
-  publishKey: 'pub-c-eb5c4104-ca7a-4538-b17f-b8b5b2924166',
-  subscribeKey: 'sub-c-3fa20908-e29e-11e9-89da-5a5bbf30aaae',
+  publishKey: '<INSERT PUBLISH KEY>',
+  subscribeKey: '<INSERT SUBSCRIBE KEY>',
   subscribeRequestTimeout: 60000,
   presenceTimeout: 122
 })
